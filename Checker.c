741663de87801677f7cf1b40d9d77e65c695b764
AiKaoWaFake <wuttipatara.arpa@mail.kmutt.ac.th>
2026-05-09 21:42:58 +0700

    Add files via upload

---
+++ Checker.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 100
+
+typedef struct Node{
+    char course[50];
+    char minGrade;
+    struct Node* next;
+} Node;
+
+typedef struct {
+    char course[50];
+    Node* prereqList;
+} Course;
+
+int findCourseIndex()
+
+int addPrereq()
+
+int findPrerequisites()
+
+void displayCourses()
+
+void something about data???
+
+int main(){
+    present choices
+    input choice
+    program run
+    you are eligible!!! 
+}
